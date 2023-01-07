7f9acfd705aa86b8fe5420856dad1e201e26a7cf
Hari Krishnan <62686776+thefitbea@users.noreply.github.com>
2023-01-08 00:14:56 +0530

    Add files via upload

---
+++ sum.py
@@ -0,0 +1,11 @@
+print(23+7)
+
+#**************************
+
+
+num1= input("Enter first number")
+num2 = input("Enter second number")
+
+sum=float(num1)+float(num2)
+print("The total is "+str(sum))
+print(2+3)
